fbc279e16feaa8e9367af792cc63eb162a8841a3
isurukavinda99 <isurukavinda999@gmail.com>
2025-08-07 21:30:02 +0530

    update game

--- app/repositories/game_repository.py
+++ app/repositories/game_repository.py
@@ -1,4 +1,6 @@
 from sqlalchemy.orm import Session, joinedload
+
+from app.dto.game_schema import GameUpdate
 from app.entity.game_entity import Game as GameModal
 
 class GameRepository:
@@ -21,3 +23,31 @@ class GameRepository:
     @staticmethod
     def get_by_id(game_id: int, db: Session):
         return db.query(GameModal).options(joinedload(GameModal.category)).filter(GameModal.id == game_id).first()
+
+
+    @staticmethod
+    def update_game(invoker: str, game_id: int, update_data: GameUpdate, db: Session):
+        game = db.query(GameModal).filter(GameModal.id == game_id).first()
+
+        if not game:
+            return None  # This is checked in the service layer
+
+        # Update only the fields provided in update_data
+        if update_data.name is not None:
+            game.name = update_data.name
+
+        if update_data.category_id is not None:
+            game.category_id = update_data.category_id
+
+        if update_data.active is not None:
+            game.active = update_data.active
+
+        game.updated_by = invoker
+
+        try:
+            db.commit()
+            db.refresh(game)
+            return game
+        except Exception as e:
+            db.rollback()
+            raise e
